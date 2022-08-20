bcc20b6cdea3f5cc4f550f95b709e96858580626
3rooks <Brookslynx@gmail.com>
2022-08-20 02:21:55 -0300

    🚧 Compression Working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-import { printHello } from '#lib/hello.js';
+import {
+    assertDirectoryWritable,
+    assertFileReadable
+} from '#lib/assert-file.js';
+import { createReadStream, createWriteStream } from 'fs';
+import { unlink } from 'fs/promises';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream';
+import { fileURLToPath } from 'url';
+import { createGzip, constants } from 'zlib';
 
-printHello();
+const inputFile = 'video.mp4';
+
+const inputPathFile = join(
+    dirname(fileURLToPath(import.meta.url)),
+    '../data',
+    inputFile
+);
+
+const outputFile = `${inputFile}.gz`;
+
+const outputPath = join(dirname(fileURLToPath(import.meta.url)), '../out');
+
+const outputPathFile = join(outputPath, outputFile);
+
+const bootstrap = async () => {
+    await assertFileReadable(inputPathFile);
+    await assertDirectoryWritable(outputPath);
+
+    const readFileStream = createReadStream(inputPathFile);
+
+    const gzipStream = createGzip({
+        level: constants.Z_BEST_COMPRESSION
+    });
+
+    const writeFileStream = createWriteStream(outputPathFile);
+
+    pipeline(readFileStream, gzipStream, writeFileStream, async (err) => {
+        if (err) {
+            try {
+                await unlink(outputPathFile);
+            } catch (err) {}
+            console.log('Compression aborted, an error has ocurred', err);
+            process.exit(1);
+        } else {
+            console.log('Compression finished');
+            process.exit();
+        }
+    });
+};
+bootstrap();
